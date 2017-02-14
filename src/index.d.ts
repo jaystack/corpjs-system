0728d5741275f9514b99d38d58b02fc423d2ef58
Péter Hauszknecht <peter.hauszknecht@jaystack.com>
2017-02-14 12:35:02 +0100

    fixes

--- src/index.d.ts
+++ src/index.d.ts
@@ -18,7 +18,7 @@ export interface Dependency {
 
 export interface Definition {
   name: string
-  component: Component
+  component: Component<any>
   dependencies: Dependency[]
 }
 
@@ -29,15 +29,15 @@ export interface Definitions {
 export default class System {
   name: string
   bootstrap(path: string): System
-  configure(component: Component): System
-  add(componentName: string, component: Component, options: Object): System
-  set(componentName: string, component: Component, options: Object): System
+  configure(component: Component<any>): System
+  add(componentName: string, component: Component<any>, options: Object): System
+  set(componentName: string, component: Component<any>, options: Object): System
   remove(componentName: string): System
   include(System): System
   merge(System): System
   dependsOn(...dependencies: (string | Dependency)[]): System
-  start(cb: Callback<Component[]>): System
-  stop(cb: Callback<Component[]>): System
-  restart(cb: Callback<Component[]>): System
+  start(cb: Callback<Component<any>[]>): System
+  stop(cb: Callback<Component<any>[]>): System
+  restart(cb: Callback<Component<any>[]>): System
   definitions: Definitions
 }
