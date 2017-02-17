04435c9d31cc252d448e56a77f4c2d23a97e4d2f
szabof-JS <szabof@jaystack.com>
2017-02-17 10:21:45 +0100

    Remove unused code

--- test_src/index.filterResources.spec.ts
+++ test_src/index.filterResources.spec.ts
@@ -95,35 +95,3 @@ describe('corpjs-system / filterResources', () => {
   })
 
 })
-
-function config() {
-  return {
-    async start(deps) {
-      return { timeout: 100 }
-    },
-    async stop() {
-      console.log('stop config')
-    }
-  }
-}
-
-function something() {
-  let timeout
-  return {
-    async start({config}) {
-      timeout = config.timeout
-      await sleep(timeout)
-      return { yeee: 'yeee' }
-    },
-    async stop() {
-      console.log('stop something')
-      return await sleep(timeout)
-    }
-  }
-}
-
-function sleep(timeout): Promise<void> {
-  return new Promise<void>((resolve) => {
-    setTimeout(() => resolve(), timeout)
-  })
-}
