a8928d331145a24dc0e8ec3f690c3f8f97b42ed8
szabof-JS <szabof@jaystack.com>
2017-02-17 10:02:18 +0100

    Finish unit tests for filterResources in index.ts

--- test_src/index.filterResources.spec.ts
+++ test_src/index.filterResources.spec.ts
@@ -13,6 +13,70 @@ describe('corpjs-system / filterResources', () => {
 
   it('should work', () => {
     const resources: System.ResourceDescriptor = {
+      "ResourceA": "A",
+      "ResourceB": "B"
+    }
+    const dependencies: System.Dependency[] = [
+      { component: "ResourceA", as: "ResourceA" },
+      { component: "ResourceB", as: "ResourceB" },
+    ]
+    const expectedResourcesResult: System.ResourceDescriptor = {
+      "ResourceA": "A",
+      "ResourceB": "B"
+    }
+    const resourcesResult = filterResources(resources, dependencies)
+    assert.deepEqual(expectedResourcesResult, resourcesResult)
+  })
+
+  it('missing element in resources should be skipped', () => {
+    const resources: System.ResourceDescriptor = {
+      "ResourceA": "A",
+    }
+    const dependencies: System.Dependency[] = [
+      { component: "ResourceA", as: "ResourceA" },
+      { component: "ResourceB", as: "ResourceB" },
+    ]
+    const expectedResourcesResult: System.ResourceDescriptor = {
+      "ResourceA": "A",
+    }
+    const resourcesResult = filterResources(resources, dependencies)
+    assert.deepEqual(expectedResourcesResult, resourcesResult)
+  })
+
+  it('missing element in dependencies should be skipped', () => {
+    const resources: System.ResourceDescriptor = {
+      "ResourceA": "A",
+      "ResourceB": "B"
+    }
+    const dependencies: System.Dependency[] = [
+      { component: "ResourceA", as: "ResourceA" },
+    ]
+    const expectedResourcesResult: System.ResourceDescriptor = {
+      "ResourceA": "A",
+    }
+    const resourcesResult = filterResources(resources, dependencies)
+    assert.deepEqual(expectedResourcesResult, resourcesResult)
+  })
+
+  it('should resolve source', () => {
+    const resources: System.ResourceDescriptor = {
+      "ResourceA": { "SourceA": "A" },
+      "ResourceB": "B"
+    }
+    const dependencies: System.Dependency[] = [
+      { component: "ResourceA", as: "A", source: "SourceA" },
+      { component: "ResourceB", as: "ResourceB" },
+    ]
+    const expectedResourcesResult: System.ResourceDescriptor = {
+      "A": "A",
+      "ResourceB": "B"
+    }
+    const resourcesResult = filterResources(resources, dependencies)
+    assert.deepEqual(expectedResourcesResult, resourcesResult)
+  })
+
+  it('should skip missing elements on both sides', () => {
+    const resources: System.ResourceDescriptor = {
       "ResourceMissingFromDependencies": "Missing",
       "ResourceA": { "SourceA": "A" },
       "ResourceB": "B"
